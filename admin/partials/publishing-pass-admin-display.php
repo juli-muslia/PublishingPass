500b3855686a42beb0896ad63cfa2d76d5c88f62
Julian Muslia <10553138+juli-muslia@users.noreply.github.com>
2022-09-02 15:24:58 +0200

    Added EXPORT button
    Added Export button in order to export all data to an Excel file.

--- admin/partials/publishing-pass-admin-display.php
+++ admin/partials/publishing-pass-admin-display.php
@@ -174,6 +174,10 @@ function crudPublishingPass()
                       ADD DATA
                   </button>
 
+                  <button type="button" class="btn btn-warning " id="export-button">
+                      EXPORT
+                  </button>
+
                   <?php
 
 
@@ -296,7 +300,26 @@ function crudPublishingPass()
         });
     });
 </script>
+<script>
+function html_table_to_excel(type)
+    {
+        var data = document.getElementById('datatableid');
 
+        var file = XLSX.utils.table_to_book(data, {sheet: "sheet1"});
+
+        XLSX.write(file, { bookType: type, bookSST: true, type: 'base64' });
+
+        XLSX.writeFile(file, 'file.' + type);
+    }
+
+    const export_button = document.getElementById('export-button');
+
+    export_button.addEventListener('click', () =>  {
+        html_table_to_excel('xlsx');
+    });
+
+
+</script>
 
 
 </body>
